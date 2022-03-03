c3d7830dc0fc7d1210f5a5f2b124417ce74f44ab
zeromountain <sonyeongsan@gmail.com>
2022-03-04 02:35:33 +0900

    fix: change redux store

--- src/store/review/reviewSlice.js
+++ src/store/review/reviewSlice.js
@@ -2,19 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   data: null,
-  form: {
-    content: '',
-  },
 };
 
 const reviewSlice = createSlice({
   name: 'review',
   initialState,
-  reducers: {
-    initializeForm(state) {
-      state.form.content = initialState.form.content;
-    },
-  },
+  reducers: {},
   extraReducers: {},
 });
 
