02c1b166070fd7bba1cbb9679c6829d8cea87823
zeromountain <sonyeongsan@gmail.com>
2022-03-03 22:22:14 +0900

    styles: Whole Container width 500px

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-// const WholeContainer = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   overflow-y: scroll;
-// `;
+import styled from 'styled-components';
 
 import DetailPage from './components/pages/DetailPage';
 import Home from './components/pages/Home';
@@ -14,13 +9,21 @@ import Register from './components/pages/Register';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/detail/:id" element={<DetailPage />} />
-      </Routes>
+      <WholeContainer>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+        </Routes>
+      </WholeContainer>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+const WholeContainer = styled.div`
+  width: 500px;
+  margin: 0 auto;
+  overflow-y: scroll;
+`;
