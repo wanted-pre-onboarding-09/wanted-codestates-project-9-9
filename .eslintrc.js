2bfe9666ce9ae5e255ed0bcc352ae73a905bfdc5
zeromountain <sonyeongsan@gmail.com>
2022-03-04 02:36:15 +0900

    chore: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,5 +36,7 @@ module.exports = {
     'no-param-reassign': 'off',
     'no-shadow': 'off',
     'consistent-return': 'off',
+    'jsx-a11y/img-redundant-alt': 'off',
+    'react/no-array-index-key': 'off',
   },
 };
