0e265bf01040ad34c1887a884cc9633860981f29
zeromountain <sonyeongsan@gmail.com>
2022-03-04 14:33:53 +0900

    feat: add loading spinner at home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { RotatingLines } from 'react-loader-spinner';
 import { latestOrder } from '../../store/review/reviewSlice';
 import Filter from '../atmoms/Filter';
 import ListPage from '../molecules/ListPage';
@@ -16,51 +18,75 @@ import DetailDelivery from '../organisms/detail/DetailDelivery';
 import ShareItems from '../atmoms/ShareItems';
 import OneClickTop from '../atmoms/OneClickTop';
 import CommentContainer from '../organisms/CommentContainer';
-
-const WholeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+import RegisterButton from '../atmoms/register/RegisterButton';
 
 function Home() {
   const [page, setPage] = useState(true);
   const detailPageData = useSelector((state) => state.review.data);
   const modalValue = useSelector((state) => state.isOpenModal.openValue);
   const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    const loader = setTimeout(() => setLoading(!loading), 3000);
     dispatch(latestOrder());
+
+    return () => clearTimeout(loader);
   }, []);
+
   if (page) {
     return (
       <WholeContainer>
-        <Filter />
+        <Wrapper>
+          <Filter />
+          <Link to="/register">
+            <RegisterButton />
+          </Link>
+        </Wrapper>
         <ListBtn page={page} setPage={setPage} />
-        <ListPage />
-        &times;
+        {loading ? (
+          <div className="loading-spinner__wrapper">
+            <RotatingLines />
+          </div>
+        ) : (
+          <ListPage />
+        )}
       </WholeContainer>
     );
   }
   return (
     <WholeContainer>
-      <Filter />
+      <Wrapper>
+        <Filter />
+        <Link to="/register">
+          <RegisterButton />
+        </Link>
+      </Wrapper>
       <ListBtn page={page} setPage={setPage} />
       <OneClickTop />
       {modalValue ? <ShareItems /> : ''}
-      {detailPageData.map((dataset) => (
-        <div key={dataset.postNumber}>
-          <DetailTop data={dataset} />
-          <DetailImg data={dataset} />
-          <DetailButton data={dataset} />
-          <DetailRating data={dataset} />
-          <DetailBuyOption />
-          <DetailContent data={dataset} />
-          <DetailReviewSwiper data={dataset} />
-          <DetailDelivery data={dataset} />
-          <CommentWrapper>
-            <CommentContainer review={dataset} />
-          </CommentWrapper>
+      {loading ? (
+        <div className="loading-spinner__wrapper">
+          <RotatingLines />
         </div>
-      ))}
+      ) : (
+        detailPageData.map((dataset) => (
+          <div key={dataset.postNumber}>
+            <DetailTop data={dataset} />
+            <DetailImg data={dataset} />
+            <DetailButton data={dataset} />
+            <DetailRating data={dataset} />
+            <DetailBuyOption />
+            <DetailContent data={dataset} />
+            <DetailReviewSwiper data={dataset} />
+            <DetailDelivery data={dataset} />
+            <CommentWrapper>
+              <CommentContainer review={dataset} />
+            </CommentWrapper>
+          </div>
+        ))
+      )}
     </WholeContainer>
   );
 }
@@ -81,8 +107,24 @@ export default Home;
 //   }
 //   -ms-overflow-style: none; /* IE and Edge */
 // `;
+const WholeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  .loading-spinner__wrapper {
+    position: absolute;
+    top: 400px;
+    left: 200px;
+  }
+`;
 const CommentWrapper = styled.div`
   width: 100%;
   padding: 18px 25px;
   background-color: #f9f9f9;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
