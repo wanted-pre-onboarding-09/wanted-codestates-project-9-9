8788e67a1c9a9e20988080f5b95640fbe11ea4dc
ysh2987 <ysh2987@naver.com>
2022-03-04 21:33:43 +0900

    build error modify

--- src/components/organisms/register/RegisterForm.js
+++ src/components/organisms/register/RegisterForm.js
@@ -14,7 +14,6 @@ function RegisterForm() {
   const { content } = useSelector(({ form }) => form);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [files, setFiles] = useState(null);
   const [image, setImage] = useState([]);
   const [rating, setRating] = useState(3);
   const starArr = [1, 2, 3, 4, 5];
@@ -44,7 +43,6 @@ function RegisterForm() {
 
     if (files) {
       [].forEach.call(files, readAndPreview);
-      setFiles(files);
     }
   };
   const handleSubmit = (e) => {
